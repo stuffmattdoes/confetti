b1adc729559180c3c63994c32ef61f229cf1d914
Matthew J. Morrison <matt@ascentus.com>
2020-01-14 00:36:27 -0500

    memory leak somewhere

--- confetti.js
+++ confetti.js
@@ -1,48 +1,32 @@
 const HALF_PI = Math.PI * 0.5;
 const colors = [
-    // blue 
-    // '#f2faff', 
-    // '#c2e3fc', 
-    '156, 206, 247',    // #9ccef7
-    '110, 179, 240',    // #6eb3f0
-    '64, 148, 227',     // #4094e3
-    '23, 115, 207',     // #1773cf
-    '5, 89, 173',       // #0559ad
-    '0, 68, 135',       // #004487
-    '5, 51, 97',        // #053361
-    '13, 40, 66',       // #0d2842
-
-    // yellow
-    // '#fffde5', 
-    // '#fff9c0', 
-    '255, 245, 149',    // #fff595
-    '254, 241, 103',    // #fef167
-    '252, 236, 63',     // #fcec3f
-    '250, 231, 0',      // #fae700
-    '255, 192, 0',      // #ffc000
-    '255, 167, 0',      // #ffa700
-    // '#fe7a00',
-    // '#f05107'
+    '244, 124, 124',    // #F47C7C
+    '247, 244, 139',    // #F7F48B
+    '161, 222, 147',    // #A1DE93
+    '112, 161, 215',    // #70A1D7
 ];
 
-function Pool () {
+function Pool(obj, count) {
     this.active = [];
-    this.inactive = [];
+    this.inactive = new Array(count);
+    this.inactive.fill(obj, 0, count);
 }
 
 Pool.prototype = {
-    pool: function () {
-        console.log('Spawn!');
+    despawn: function () {
+        console.log('Despawn!');
     },
     spawn: function () {
         console.log('Spawn!');
+        return this.inactive.length ? this.inactive[0] : null
     }
 }
 
-let _particles = [],
-    animId,
+let animId,
+    // explosions = new Pool(new Explosion(0, 0, 300), 15),
+    explosions = [],
     viewWidth = window.innerWidth,
-    viewHeight = 250,
+    viewHeight = window.innerHeight,
     canvas,
     ctx,
     timeStep = (1 / 60);
@@ -54,19 +38,18 @@ function Point (x, y) {
     }
 }
 
-function Particle (x, y, duration, velocity, repeat) {
-    const { p0, p1, p2, p3 } = createPath(x, y, velocity);
+function Particle (x, y, velocity) {
+    const { p0, p1, p2, p3 } = createPath(x, y, this.velocity);
     this.p0 = p0;
     this.p1 = p1;
     this.p2 = p2;
     this.p3 = p3;
 
     // Lifecycle
-    this.cycles = 0;
-    this.maxCycles = 2;
     this.time = 0;
-    this.duration = duration || 3 + Math.random() * 2;
-    
+    this.complete = false;
+    this.duration = 3 + Math.random() * 2;
+
     // Properties
     let rnd = Math.random();
     // this.alpha = 1.0;
@@ -75,13 +58,10 @@ function Particle (x, y, duration, velocity, repeat) {
     this.w = 6 + (Math.random() * 6);
     this.h = 6 + (Math.random() * 6);
     this.shape = Math.random() < 0.5 ? 'triangle' : 'square';
-
-    this.repeat = repeat;
-    this.complete = false;
 };
 
 Particle.prototype = {
-    draw: function() {
+    draw: function () {
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(this.r);
@@ -104,15 +84,7 @@ Particle.prototype = {
 
         ctx.restore();
     },
-    reset: function(x, y) {
-        this.time = 0;
-        this.cycles += 1;
-
-        if (this.cycles === this.maxCycles) {
-            this.repeat = false;
-        }
-    },
-    update: function() {
+    update: function () {
         this.time = Math.min(this.duration, this.time + timeStep);
 
         let f = Ease.outCubic(this.time, 0, 1, this.duration);
@@ -128,13 +100,33 @@ Particle.prototype = {
 
         this.complete = this.time === this.duration;
 
-        if (this.complete && this.repeat) {
-            this.reset();
-        }
-
         this.draw();
     }
 };
+
+function Explosion(x, y, velocity) {
+    this.complete = false;
+    this.particles = new Array(10);
+    
+    for (let i = 0; i < this.particles.length; i++) {
+        this.particles.push(new Particle(x, y, velocity));
+    }
+
+    console.log(this.particles.length);
+
+    // this.x = x;
+    // this.x = y;
+}
+
+Explosion.prototype = {
+    update: function () {
+        // this.particles.forEach((p, i) => {
+        //     if (!p.complete) {
+        //         p.update();
+        //     }
+        // });
+    }
+}
 
 function Streamer (x, y, duration, velocity, repeat, debug) {
     // Path
@@ -271,65 +263,32 @@ Streamer.prototype = {
     }
 }
 
-// Functions
-
 // bezier-curve interpolation
 function createPath(x, y, velocity) {
+    function randomDir(velocity) {
+        const leftOrRight = Math.random() < 0.5 ? -1 : 1;
+        return Math.random() * velocity * leftOrRight;
+    }
+    
     return {
         p0: new Point(x, y),
         p1: new Point(x + randomDir(velocity), y + randomDir(velocity)),
         p2: new Point(x + randomDir(velocity), y + randomDir(velocity)),
-        p3: new Point(x + randomDir(velocity), viewHeight + 8)
+        p3: new Point(x + randomDir(velocity), y + 100 + Math.random() * 250)
     }
 }
 
 function loop() {
     ctx.clearRect(0, 0, viewWidth, viewHeight);
-    _particles.forEach((p, i) => {
-        if (p.complete && !p.repeat) {
-            _particles.splice(i, 1);
-        } else {
-            p.update();
-        }
-    });
+    // explosions.forEach((e, i) => {
+    //     if (e.complete) {
+    //         // Pool it!
+    //     } else {
+    //         e.update();
+    //     }
+    // });
 
     animId = requestAnimationFrame(loop);
-}
-
-function makeItRain() {
-    let particleRate = viewWidth / 5;
-
-    for (let i = 0; i < particleRate; i++) {
-        let x = Math.random() * viewWidth,
-            y = 0,
-            duration = 3 + Math.random() * 10,
-            nextParticle = Math.random() < 0.9 ? 
-                new Particle(x, y, duration, 100, true, false)
-                : new Streamer(x, y, duration, 200, true, false);
-
-            _particles.push(nextParticle);
-    }
-}
-
-function randomDir(velocity) {
-    const leftOrRight = Math.random() < 0.5 ? -1 : 1;
-    return Math.random() * velocity * leftOrRight;
-}
-
-function YATE() {
-    let y = viewHeight * 0.5;
-    YEET(viewWidth * 0.4, y, null, 200, false, false)
-    setTimeout(() => YEET(viewWidth * 0.5, y, null, 200, false, false), 300);
-    setTimeout(() => YEET(viewWidth * 0.6, y, null, 200, false, false), 600);
-}
-
-function YEET(x, y) {
-    for (let i = 0; i < 64; i++) {
-        let nextParticle = Math.random() < 0.9 ? 
-            new Particle(x, y, null, 200, false, false)
-            : new Streamer(x, y, null, 200, false, false)
-        _particles.push(nextParticle);
-    }
 }
 
 // Easing functions
@@ -342,29 +301,29 @@ function YEET(x, y) {
  * d = duration
  */
 let Ease = {
-    inCubic: function (t, b, c, d) {
+    inCubic: (t, b, c, d) => {
         t /= d;
         return c * t * t * t + b;
     },
-    outCubic: function(t, b, c, d) {
+    outCubic: (t, b, c, d) => {
         t /= d;
         t--;
         return c * (t * t * t + 1) + b;
     },
-    inOutCubic: function(t, b, c, d) {
+    inOutCubic: (t, b, c, d) => {
         t /= d / 2;
         if (t < 1) return c / 2 * t * t * t + b;
         t -= 2;
         return c / 2 * (t * t * t + 2) + b;
     },
-    inBack: function (t, b, c, d, s) {
+    inBack: (t, b, c, d, s) => {
         s = s || 1.70158;
         return c * (t /= d) * t * ((s + 1) * t - s) + b;
     }
 };
 
 const Bezier = {
-    cubic: function(p0, c0, c1, p1, t) {
+    cubic: (p0, c0, c1, p1, t) => {
         let p = new Point();
         let nt = (1 - t);
 
@@ -379,7 +338,7 @@ const Bezier = {
     
         return p;
     },
-    equidistant: function(p0, c0, c1, p1, t) {
+    equidistant: (p0, c0, c1, p1, t) => {
         let p = new Point();
         const t2 = t * t;
         const t3 = t2 * t;
@@ -395,33 +354,7 @@ const Bezier = {
         + p1.y * t3;
 
         return p;
-    },
-    // equidistant2: function(p0, c0, c1, p1, t) {
-    //     var targetLength = u * this.arcLengths[this.len];
-    //     var low = 0, high = this.len, index = 0;
-        
-    //     while (low < high) {
-    //         index = low + (((high - low) / 2) | 0);
-    //         if (this.arcLengths[index] < targetLength) {
-    //             low = index + 1;
-
-    //         } else {
-    //             high = index;
-    //         }
-    //     }
-        
-    //     if (this.arcLengths[index] > targetLength) {
-    //         index--;
-    //     }
-
-    //     var lengthBefore = this.arcLengths[index];
-    //     if (lengthBefore === targetLength) {
-    //         return index / this.len;
-
-    //     } else {
-    //         return (index + (targetLength - lengthBefore) / (this.arcLengths[index + 1] - lengthBefore)) / this.len;
-    //     }
-    // }
+    }
 }
 
 function pythagorean(p1, p2) {
@@ -454,29 +387,16 @@ function setDimensions() {
     canvas.height = viewHeight;
 }
 
-function destroy() {
-    if (!canvas) {
-        return;
-    }
-
-    canvas.removeEventListener('click', event => YEET(event.clientX, event.clientY - 48, 300));
-    window.removeEventListener('resize', init);
-    _particles = [];
-    animId && cancelAnimationFrame(animId);
-}
-
 function init() {
+    console.log('init');
     canvas = document.querySelector('canvas');
     ctx = canvas.getContext('2d');
     setDimensions();
-    makeItRain();
-    YATE();
-    canvas.addEventListener('click', event => YEET(event.clientX, event.clientY - 48, 300));
+
+    // makeItRain();
+    canvas.addEventListener('click', ({ x, y }) => explosions.push(new Explosion(x, y, 300)));
     window.addEventListener('resize', setDimensions);
     requestAnimationFrame(loop);
 }
 
-export {
-    destroy,
-    init
-}
+init();
